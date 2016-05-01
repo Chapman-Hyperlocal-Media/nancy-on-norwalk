c89f9c56361bd20cd062790f7fe5881720eb7c83
Eric Chapman <hurricane.chapman@gmail.com>
2016-04-30 22:53:16 -0400

    ads-top-of-content Formatting change.

--- wp-content/themes/norwalk/single.php
+++ wp-content/themes/norwalk/single.php
@@ -10,18 +10,20 @@
 get_header(); ?>
 <div id="main-content">
 	<div class="content">
-		<!-- /1732998/nancy_on_norwalk_top_of_content_2 -->
-		<div id='div-gpt-ad-1462049414903-0' class="top-content-ad top-content-ad-1" style='height:250px; width:300px;'>
-			<script type='text/javascript'>
-				googletag.cmd.push(function() { googletag.display('div-gpt-ad-1462049414903-0'); });
-			</script>
-		</div>
+		<div class="top-content-ads-container">
+			<!-- /1732998/nancy_on_norwalk_top_of_content_2 -->
+			<div id='div-gpt-ad-1462049414903-0' class="top-content-ad top-content-ad-1" style='height:250px; width:300px;'>
+				<script type='text/javascript'>
+					googletag.cmd.push(function() { googletag.display('div-gpt-ad-1462049414903-0'); });
+				</script>
+			</div>
 
-		<!-- /1732998/Norwalk_top_of_content_1 -->
-		<div id='div-gpt-ad-1462049414903-1' class="top-content-ad top-content-ad-2" style='height:250px; width:300px;'>
-			<script type='text/javascript'>
-				googletag.cmd.push(function() { googletag.display('div-gpt-ad-1462049414903-1'); });
-			</script>
+			<!-- /1732998/Norwalk_top_of_content_1 -->
+			<div id='div-gpt-ad-1462049414903-1' class="top-content-ad top-content-ad-2" style='height:250px; width:300px;'>
+				<script type='text/javascript'>
+					googletag.cmd.push(function() { googletag.display('div-gpt-ad-1462049414903-1'); });
+				</script>
+			</div>
 		</div>
 		<?php get_template_part( 'loop', 'single' ); ?>
 	</div>
