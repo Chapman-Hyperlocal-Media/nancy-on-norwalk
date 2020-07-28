ab0fe7fb6b0f1e6d89460c76af92b179acbc4908
Eric Chapman <hurricane.chapman@gmail.com>
2020-07-27 21:45:33 -0400

    release-1.3.7 Improvement to ad code, to eliminate unnecessary refresh on page load. Also adds eslint. (#4)
    * release-1.3.7 Improvement to ad code, to eliminate unnecessary refresh on page load
    
    * release-1.3.7 Stops gulp from processing modernizr. Adds real babel config, and uglifies js for production.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 sass.compiler = require('sass');
 
 const themePath = 'wp-content/themes/norwalk/';
@@ -14,56 +15,77 @@ const cssRegex = /.*\.css$/;
 const jsMinRegex = /.*\.min\.js$/;
 
 const cleanJS = (cb) => {
-	fs.readdirSync(jsPath + 'min/')
-		.filter(f => jsMinRegex.test(f))
-		.map(f => fs.unlinkSync(jsPath + 'min/' + f));
-	cb();
-}
-
-const cleanCSS = (cb) => {
-	fs.readdirSync(cssPath)
-		.filter(f => cssRegex.test(f))
-		.map(f => fs.unlinkSync(cssPath + f))
-	fs.readdirSync(cssPath + 'maps/')
-		.map(f => fs.unlinkSync(cssPath + 'maps/' + f));
-	fs.rmdirSync(cssPath + 'maps');
-	cb();
-}
-
-const developSass = (dev = false) => src(sassPath + '*.scss')
-	.pipe(sourcemaps.init())
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(sourcemaps.write('./maps'))
-	.pipe(dest(cssPath));
-
-const developJS = (dev = false) => src(jsPath + '*.js')
-	.pipe(sourcemaps.init())
-	.pipe(babel())
-	.pipe(sourcemaps.write('./maps'))
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(dest(jsPath + 'min/'));
-
-exports.develop = () => {
-	watch(sassPath + '*.scss', developSass);
-	watch(jsPath + '*.js', developJS);
+    fs.readdirSync(jsPath + 'min/')
+        .filter(f => jsMinRegex.test(f))
+        .map(f => fs.unlinkSync(jsPath + 'min/' + f));
+    try {
+        fs.readdirSync(jsPath + 'min/maps/')
+            .map(f => fs.unlinkSync(jsPath + 'min/maps/' + f));
+        fs.rmdirSync(jsPath + 'min/maps');
+    }
+    catch(e) {
+        console.log('\x1b[33m', 'This is probably just that /maps/ doesn\'t exist, which is fine.', "\x1b[0m");
+        console.error(e);
+    }
+    cb();
 };
 
-const compileJS = (dev = false) => src(jsPath + '*.js')
-	.pipe(babel())
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(dest(jsPath + 'min/'));
+const cleanCSS = (cb) => {
+    fs.readdirSync(cssPath)
+        .filter(f => cssRegex.test(f))
+        .map(f => fs.unlinkSync(cssPath + f));
+    try {
+        fs.readdirSync(cssPath + 'maps/')
+            .map(f => fs.unlinkSync(cssPath + 'maps/' + f));
+        fs.rmdirSync(cssPath + 'maps');
+    }
+    catch(e) {
+        console.log('\x1b[33m', 'This is probably just that /maps/ doesn\'t exist, which is fine.', "\x1b[0m");
+        console.error(e);
+    }
+    cb();
+};
 
-const compileSass = (dev = false) => src(sassPath + '*.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(dest(cssPath));
+const developSass = () => src(sassPath + '*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(dest(cssPath));
+
+const developJS = () => src(jsPath + '*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(dest(jsPath + 'min/'));
+
+exports.develop = () => {
+    watch(sassPath + '*.scss', developSass);
+    watch(jsPath + '*.js', developJS);
+};
+
+const compileJS = () => src(jsPath + '*.js', {ignore: 'modernizr.*.js'})
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(dest(jsPath + 'min/'));
+
+const copyModernizr = () => src(jsPath + 'modernizr.*.js')
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(dest(jsPath + 'min/'));
+
+const compileSass = () => src(sassPath + '*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(dest(cssPath));
 
 exports.sass = series(cleanCSS, compileSass);
-exports.js = series(cleanJS,  compileJS);
+exports.js = series(cleanJS, compileJS);
 exports.build = series(
-		cleanJS,
-		cleanCSS,
-		parallel(
-			compileSass,
-			compileJS
-		)
-	);
+    cleanJS,
+    cleanCSS,
+    parallel(
+        compileSass,
+        compileJS,
+        copyModernizr
+    )
+);
